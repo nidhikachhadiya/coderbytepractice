9a1c101125323b1f0d5db4f4f8de8043a3900a52
nidhikachhadiya <nidhi3578@gmail.com>
2023-09-01 11:07:05 -0400

    add code for thread

--- src/com/practice/thread/ThreadExampleWithRunnableInterface.java
+++ src/com/practice/thread/ThreadExampleWithRunnableInterface.java
@@ -1,4 +1,17 @@
 package com.practice.thread;
 
-public class ThreadExampleWithRunnableInterface {
+public class ThreadExampleWithRunnableInterface implements Runnable
+{
+    public static void main(String[] args) {
+        for(int i=0;i<10;i++) {
+            Thread thread = new Thread(new ThreadExampleWithRunnableInterface());
+            thread.start();
+
+        }
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getId());
+    }
 }
